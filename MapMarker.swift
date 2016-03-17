f6646553018fb0d5f917aa73a612f7aa02964c46
Usuário Convidado <Guest@l2304micro08.local>
2016-03-17 19:52:11 -0300

    Dir. Models Modelo do marker

---
+++ MapMarker.swift
@@ -0,0 +1,25 @@
+//
+//  MapMarker.swift
+//  MapBase
+//
+//  Created by Usuário Convidado on 17/03/16.
+//  Copyright © 2016 Map Base 5. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapMarker: NSObject, MKAnnotation {
+    
+    var coordinate: CLLocationCoordinate2D
+    var title:String?
+    var subtitle:String?
+    
+    init(coordinate:CLLocationCoordinate2D, title: String, subtitle: String){
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+    }
+    
+    
+}
